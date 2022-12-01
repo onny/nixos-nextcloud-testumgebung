12b499c8e40f5c3afbaa86ade7305994c0f6f40e
Jonas Heinrich <onny@project-insanity.org>
2022-12-01 17:52:10 +0100

    add flake config

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          php82Packages.composer
+          nixos-shell
+          nodejs
+          # phpunit
+        ];
+      };
+    };
+}
+
