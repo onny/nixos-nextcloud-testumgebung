5854235d65686fef8fdf5464e5726b63c9f3be46
Jonas Heinrich <onny@project-insanity.org>
2022-12-01 17:42:59 +0100

    add devbox config

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	QEMU_NET_OPTS="hostfwd=tcp::80-:80" NIX_PATH=nixpkgs=/home/onny/projects/nixpkgs nixos-shell vm-nextcloud.nix
+	sudo -E QEMU_NET_OPTS="hostfwd=tcp::80-:80" NIX_PATH=nixpkgs=/home/onny/projects/nixpkgs nixos-shell vm-nextcloud.nix
--- vm-nextcloud.nix
+++ vm-nextcloud.nix
@@ -8,11 +8,11 @@
     extraApps = {
       circles = pkgs.fetchNextcloudApp rec {
         url = "https://github.com/nextcloud-releases/circles/releases/download/0.21.4/circles-0.21.4.tar.gz";
-        sha256 = "sha256-sQUsYC3cco6fj9pF2l1NrCEhA3KJoOvJRhXvBlVplll=";
+        sha256 = "sha256-gkW9jZvXS86ScuM434mUbvQajYKwHVjm9PfTMNgHL/Q=";
       };
       calendar = pkgs.fetchNextcloudApp rec {
         url = "https://github.com/nextcloud-releases/calendar/releases/download/v4.1.0/calendar-v4.1.0.tar.gz";
-        sha256 = "sha256-eTc51pkg3OdHJB7X4/hD39Ce+9vKzw1nlJ7BhPOzlll=";
+        sha256 = "sha256-KALFhCNjofFQMntv3vyL0TJxqD/mBkeDpxt8JV4CPAM=";
       };
     };
   };
