8e2f5c8bdec20eb7f424fd83d9bc4181302fa355
Jonas Heinrich <onny@project-insanity.org>
2022-12-21 15:54:04 +0100

    include local calendar build

--- vm-nextcloud.nix
+++ vm-nextcloud.nix
@@ -1,4 +1,6 @@
-{ pkgs, config, lib, options, ... }: {
+{ pkgs, config, lib, options, ... }:
+  with import <nixpkgs> {};
+{
 
   nixpkgs = {
     overlays = [
@@ -29,9 +31,12 @@
         url = "https://github.com/nextcloud-releases/circles/releases/download/0.21.4/circles-0.21.4.tar.gz";
         sha256 = "sha256-gkW9jZvXS86ScuM434mUbvQajYKwHVjm9PfTMNgHL/Q=";
       };
-      calendar = pkgs.fetchNextcloudApp rec {
-        url = "https://github.com/nextcloud-releases/calendar/releases/download/v4.1.0/calendar-v4.1.0.tar.gz";
-        sha256 = "sha256-KALFhCNjofFQMntv3vyL0TJxqD/mBkeDpxt8JV4CPAM=";
+      calendar = pkgs.stdenvNoCC.mkDerivation rec {
+        name = "calendar";
+        src = /home/onny/projects/calendar;
+        unpackPhase = ''cp -r --no-preserve=mode $src/* .'';
+        dontBuild = true;
+        installPhase = ''cp -r --no-preserve=mode . $out/'';
       };
       mail = pkgs.nextcloud25Packages.apps.mail;
     };
